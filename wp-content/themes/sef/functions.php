23e8507413bb2953ee546c903dfde0923bd5c6ed
Sacha Meunier <sacha.meunier@student.hepl.be>
2024-08-25 18:32:18 +0200

    ✨ Hide admin bar

--- wp-content/themes/sef/functions.php
+++ wp-content/themes/sef/functions.php
@@ -3,3 +3,4 @@
 require_once( __DIR__ . '/includes/disable-gutenberg.php');
 require_once( __DIR__ . '/includes/enqueue-assets.php');
 require_once( __DIR__ . '/includes/title-tag.php');
+require_once( __DIR__ . '/includes/admin-bar.php');
---
+++ wp-content/themes/sef/includes/admin-bar.php
@@ -0,0 +1,4 @@
+<?php
+
+// Hide admin bar on website preview.
+add_filter('show_admin_bar', '__return_false');
