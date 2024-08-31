ad64f16b7d87bbcc1b9242a1a302ad4ca40d5ca7
Sacha Meunier <sacha.meunier@student.hepl.be>
2024-08-31 13:02:02 +0200

    Refactoring

--- wp-content/themes/sef/includes/title-tag.php
+++ wp-content/themes/sef/includes/title-tag.php
@@ -1,7 +1,9 @@
 <?php
 
 // Lets Wordpress define the title-tag for each page based on the context.
-function sef_theme_setup() {
-add_theme_support( 'title-tag' );
+function sef_theme_setup()
+{
+    add_theme_support('title-tag');
 }
-add_action( 'after_setup_theme', 'sef_theme_setup' );
+
+add_action('after_setup_theme', 'sef_theme_setup');
