6d8c329dbf32f66f8ec51cba389d3bce035340d8
Sacha Meunier <sacha.meunier@student.hepl.be>
2024-09-03 21:20:45 +0200

    Fixing dev dependencies

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,8 +6,10 @@ mix.ts('wp-content/themes/sef/public/src/ts/main.ts', 'js/main.js').sourceMaps()
     .sass('wp-content/themes/sef/public/src/scss/main.scss', 'css').sourceMaps()
     .setPublicPath('dist').setResourceRoot('../')
     .browserSync({
-        proxy: false,
-        server: {
-            baseDir: './'
-        }
+        proxy: 'dw-sef.test',
+        files: [
+            'dist/css/*.css',
+            'dist/js/*.js',
+        ],
+        notify: false,
     });
