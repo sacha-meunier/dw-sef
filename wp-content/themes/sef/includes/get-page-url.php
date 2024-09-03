470faa3b3d79c7188f4c46a30a35103b7620278e
Sacha Meunier <sacha.meunier@student.hepl.be>
2024-09-03 21:23:06 +0200

    Improved function

--- wp-content/themes/sef/includes/get-page-url.php
+++ wp-content/themes/sef/includes/get-page-url.php
@@ -1,10 +1,16 @@
 <?php
 
-function get_contact_page_url(): bool|string
+/**
+ * Get page URL by it's slug.
+ *
+ * @param string $slug The slug of the page.
+ * @return string The URL of the page or '#' if page doesn't exist.
+ */
+function get_page_url_by_slug(string $slug): string
 {
-    $contact_page = get_page_by_path('contact');
-    if ($contact_page) {
-        return get_permalink($contact_page->ID);
+    $page = get_page_by_path($slug);
+    if ($page) {
+        return get_permalink($page->ID);
     } else {
         return '#';
     }
