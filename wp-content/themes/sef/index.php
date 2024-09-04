f23daf62d4048ce459bcca98741771c13b5e0941
Sacha Meunier <sacha.meunier@student.hepl.be>
2024-09-04 06:01:42 +0200

    Refactoring

--- wp-content/themes/sef/index.php
+++ wp-content/themes/sef/index.php
@@ -1,12 +1,12 @@
 <?php get_header(); ?>
 <?php if (have_posts()): while (have_posts()): the_post(); ?>
 
-    <?php get_template_part('home/hero'); ?>
+    <?php /*get_template_part('home/hero'); */?>
     <?php /*get_template_part('home/donation'); */?>
-    <?php /*get_template_part('home/news'); */?>
+    <?php get_template_part('home/news'); ?>
     <?php /*get_template_part('home/shop'); */?>
-    <?php get_template_part('home/faq'); ?>
-    <?php get_template_part('home/support'); ?>
+    <?php /*get_template_part('home/faq'); */?>
+    <?php /*get_template_part('home/support'); */?>
 
 <?php endwhile; endif; ?>
 <?php get_footer(); ?>
